3e30c22ade9ea71a6ea7d684586b2e05ddd0205d
Kenny <101839505+khui0@users.noreply.github.com>
2024-08-10 01:19:22 -0400

    fix: https?

--- src/lib/pocketbase.ts
+++ src/lib/pocketbase.ts
@@ -2,7 +2,7 @@ import PocketBase from "pocketbase";
 
 import { writable } from "svelte/store";
 
-export const pb = new PocketBase("http://db.kennyhui.dev/");
+export const pb = new PocketBase("https://db.kennyhui.dev/");
 
 export const currentUser = writable(pb.authStore.model);
 
