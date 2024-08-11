0cce653ee568e3b5ff2cd47ee233a5814c1240f4
Kenny <101839505+khui0@users.noreply.github.com>
2024-08-10 23:41:38 -0400

    feat: improve back button behavior

--- src/routes/(auth)/+layout.svelte
+++ src/routes/(auth)/+layout.svelte
@@ -5,9 +5,14 @@
 
 <div class="m-auto w-[min(100%,500px)]">
   <div class="mb-3 grid grid-cols-[1fr_auto_1fr] items-center">
-    <a href="{base}/" class="btn btn-square btn-sm rounded-full"
-      ><LucideArrowLeft></LucideArrowLeft></a
+    <button
+      class="btn btn-square btn-sm rounded-full"
+      on:click={() => {
+        history.back();
+      }}
     >
+      <LucideArrowLeft></LucideArrowLeft>
+    </button>
     <div class="flex flex-col items-center">
       <h1 class="text-3xl font-light">TC3</h1>
     </div>
