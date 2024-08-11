f172f51c0e56d2b02b44c9f0db8e3e89d62847ab
Kenny <101839505+khui0@users.noreply.github.com>
2024-08-11 00:46:23 -0400

    feat: add account page

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -3,18 +3,22 @@
 
   import LucideLogOut from "~icons/lucide/log-out";
   import LucideLogIn from "~icons/lucide/log-in";
+  import LucideUser from "~icons/lucide/user";
 
   import { currentUser, signOut } from "$lib/pocketbase";
 </script>
 
 <header class="sticky top-0 z-10">
   <div class="flex items-center p-2 bg-base-200/80 backdrop-blur-lg rounded-box">
-    <div class="flex-1 px-2">
-      <a class="font-thin text-xl active:scale-95 transition-transform" href="/{base}">TC3</a>
+    <div class="px-2">
+      <a class="block font-thin text-xl active:scale-90 transition-transform" href="/{base}">TC3</a>
     </div>
-    <div class="flex-none flex items-center gap-2">
+    <div class="flex-1 flex items-center justify-end">
       {#if $currentUser}
-        <p>Signed in as <b>{$currentUser.username}</b></p>
+        <p class="px-2">Signed in as <b>{$currentUser.username}</b></p>
+        <a class="btn btn-ghost btn-sm rounded-full btn-square" href="{base}/account">
+          <LucideUser></LucideUser>
+        </a>
         <button class="btn btn-ghost btn-sm rounded-full" on:click={signOut}>
           <LucideLogOut></LucideLogOut> Sign out
         </button>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,3 +1,7 @@
+<svelte:head>
+  <title>TC3</title>
+</svelte:head>
+
 <main class="flex flex-col gap-3 p-3 mx-auto w-[min(100%,1000px)] h-full">
   <slot></slot>
 </main>
